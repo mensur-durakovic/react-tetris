ee9e5b1c9ac6d467c7f2eaf6b8ba8445726663c6
mdurakovic <mensurdurakovic@hotmail.com>
2019-08-15 15:12:00 +0200

    impl tetromino generate on stage

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -4,8 +4,8 @@ import {TETROMINOS} from '../tetrominos';
 
 const Cell = ({type}) => {
     return (
-        <StyledCell type={'L'} 
-            color={TETROMINOS['L'].color} />
+        <StyledCell type={type} 
+            color={TETROMINOS[type].color} />
     )
 }
 
--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -78,7 +78,7 @@ const Tetris = () => {
                     </div>
                 )
             }
-          <StartButton onClick={startGame} />
+          <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
