45a17b1abcf08d59cc33c51c771d5a0cba3f9269
mdurakovic <mensurdurakovic@hotmail.com>
2019-08-15 16:02:10 +0200

    impl rotation of tetromino

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -10,6 +10,22 @@ export const usePlayer = () => {
     collided: false,
   });
 
+  const rotate = (matrix, dir) => {
+    //make the rows to become columns
+    const rotatedTetro = matrix.map((_, index) => 
+        matrix.map(col => col[index])
+    )
+    //reverse each row to get a rotated matrix
+    if(dir > 0) return rotatedTetro.map(row => row.reverse());
+    return rotatedTetro.reverse();
+  }
+
+  const playerRotate = (stage, dir) => {
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+    setPlayer(clonedPlayer);
+  }
+
   const updatePlayerPos = ({ x, y, collided }) => {
     setPlayer(prev => ({
       ...prev,
@@ -26,5 +42,5 @@ export const usePlayer = () => {
     })
   }, [])
 
-  return [player, updatePlayerPos, resetPlayer];
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
 }
